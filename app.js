94cdfb9403f544c8e3d491f77cf67495f3f0f717
Hanzalashaik <shaikhhanzala27@gmail.com>
2023-10-19 05:59:21 +0000

    Error Handler

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-import config from "config"
-import express from "express"
-import booksRouter from "./controllers/Books/index.js"
-import "./utils/dbconnect.js"
+import config from "config";
+import express from "express";
+import booksRouter from "./controllers/Books/index.js";
+import "./utils/dbconnect.js";
 
+const POST = config.get("POST");
 
-const POST=config.get("POST");
-
-const app=express();
+const app = express();
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.status(200).send("Hello Server is Running 🚀");
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Hello Server is Running 🚀");
+});
 
-app.use("/books",booksRouter);
+app.use("/books", booksRouter);
 
-app.listen(POST,(req,res)=>{
-    console.log(`Server is running at port ${POST} 🚀`);
-    
-})
+
+//error handler
+app.use((req, res, next) => {
+  res.status(404).send("Not Found -Invalid Route");
+});
+
+app.listen(POST, (req, res) => {
+  console.log(`Server is running at port ${POST} 🚀`);
+});
